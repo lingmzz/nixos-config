6633d10dc9a195fad4263c9f9c91a9bfdbe95c47
lingmzz <ling_rc@outlook.com>
2026-02-02 21:20:36 +0800

    update

--- hosts/default.nix
+++ hosts/default.nix
@@ -4,12 +4,35 @@
   imports = [
     ./hardware-configuration.nix
     ./virtualization.nix
-    ../modules/core
-    ../modules/programs
+    ./boot.nix
+    ./networking.nix
+    ./user.nix
+    ./pipewire.nix
   ];
 
   # 系统级基础配置
   system.stateVersion = "25.11";
+
+  #Nix 设置
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+    auto-optimise-store = true;
+  };
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
+
+  time.timeZone = "Asia/Shanghai";
+
+  i18n.defaultLocale = "zh_CN.UTF-8";
+
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
 
   # 启用 Niri
   programs.niri.enable = true;
